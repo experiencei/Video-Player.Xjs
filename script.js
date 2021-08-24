8423e76a42e8b55bfbecd4bb43fc327b8d5df12b
experiencei <ayelojahighbee01@gmail.com>
2021-08-24 21:56:01 +0100

    functionality, to change the progress bar by offsetX and the offsetWidth

--- script.js
+++ script.js
@@ -51,6 +51,12 @@ function updateProgress() {
     duration.textContent = `${displayTime(video.duration)}`;
   }
   
+  // Click to seek within the video
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+  }
 
 // Volume Controls --------------------------- //
 
@@ -68,3 +74,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
